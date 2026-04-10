0d1bc0a48ab6c700a40deddcc749c79cbcff3d28
Iymona <iymonatoxiraliyeve@gmail.com>
2026-04-10 19:21:52 +0500

    second commit

--- bot.py
+++ bot.py
@@ -42,7 +42,7 @@ def handle_message(message):
     weather_info = get_weather(city)
     bot.send_message(message.chat.id, weather_info)
     
-@app.route('/' + BOT_TOKEN, methods=['POST'])
+@app.route('/webhook', methods=['POST'])
 def webhook():  
     json_str = request.get_data().decode('UTF-8')
     update = telebot.types.Update.de_json(json_str)
